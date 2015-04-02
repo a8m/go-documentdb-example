3d16ba4c00686ea23481434a4fb21e1fbe87d149
Ariel Mashraki <ariel@mashraki.co.il>
2015-04-02 20:08:30 +0300

    docs(*): document my code

--- server.go
+++ server.go
@@ -1,3 +1,4 @@
+// Example of a users REST api using DocumentDB
 package main
 
 import (
@@ -6,14 +7,17 @@ import (
 	"github.com/martini-contrib/binding"
 	"github.com/martini-contrib/render"
 	"io/ioutil"
+	"net/http"
 )
 
+// DocumentDB config
 type Config struct {
 	Url       string `json:"url"`
 	MasterKey string `json:"masterKey"`
 }
 
 func main() {
+	// Read `url` and `masterKey` from config file
 	data, err := ioutil.ReadFile("config.json")
 	if err != nil {
 		panic(err)
@@ -23,14 +27,47 @@ func main() {
 		panic(err)
 	}
 
+	// UsersDB
+	users := NewDB("test", "users", config)
+
 	// Init Martini, Middleware
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
-	// Routes, Controller
-	m.Get("/:id", func() {})
+	// Routes
+	m.Get("/", func(r render.Render) {
+		users, err := users.GetAll()
+		if err != nil {
+			r.JSON(http.StatusNotFound, err.Error())
+		} else {
+			r.JSON(200, users)
+		}
+	})
 	m.Post("/", binding.Bind(User{}), func(user User, r render.Render) {
-		r.JSON(200, user)
+		if err := users.Add(&user); err != nil {
+			r.JSON(http.StatusNotFound, err.Error())
+		} else {
+			r.JSON(201, user)
+		}
+	})
+	m.Get("/:id", func(params martini.Params, r render.Render) {
+		if user, err := users.Get(params["id"]); err != nil {
+			r.JSON(http.StatusNotFound, err.Error())
+		} else {
+			r.JSON(http.StatusOK, user)
+		}
+	})
+	m.Put("/:id", binding.Bind(User{}), func(user User, params martini.Params, r render.Render) {
+		if err := users.Update(params["id"], &user); err != nil {
+			r.JSON(http.StatusNotFound, err.Error())
+		}
+		r.JSON(http.StatusOK, user)
+	})
+	m.Delete("/:id", func(params martini.Params, r render.Render) {
+		if err := users.Remove(params["id"]); err != nil {
+			r.JSON(http.StatusNotFound, err.Error())
+		}
+		r.JSON(http.StatusNoContent, "")
 	})
 	m.Run()
 }
--- users.go
+++ users.go
@@ -1,19 +1,130 @@
 package main
 
 import (
+	. "fmt"
 	"github.com/a8m/documentdb"
 )
 
+// User document
 type User struct {
 	documentdb.Document
 	Name  string `json:"name,omitempty"`
 	Email string `json:"email,omitempty"`
 }
 
+// DB interface
 type DB interface {
 	Get(id string) *User
 	GetAll() []*User
 	Add(u *User) *User
 	Update(u *User) *User
-	Delete(id string) error
+	Remove(id string) error
+}
+
+// UsersDB implement DB interface
+type UsersDB struct {
+	Database   string
+	Collection string
+	db         *documentdb.Database
+	coll       *documentdb.Collection
+	client     *documentdb.DocumentDB
+}
+
+// Return new UserDB
+// Test if database and collection exist. if not, create them.
+func NewDB(db, coll string, config *Config) (users UsersDB) {
+	users.Database = db
+	users.Collection = coll
+	users.client = documentdb.New(config.Url, documentdb.Config{config.MasterKey})
+	// Find or create `test` db and `users` collection
+	if err := users.findOrDatabase(db); err != nil {
+		panic(err)
+	}
+	if err := users.findOrCreateCollection(coll); err != nil {
+		panic(err)
+	}
+	return
+}
+
+// Get user by given id
+func (u *UsersDB) Get(id string) (user User, err error) {
+	var users []User
+	query := Sprintf("SELECT * FROM ROOT r WHERE r.id='%s'", id)
+	if err = u.client.QueryDocuments(u.coll.Self, query, &users); err != nil || len(users) == 0 {
+		return user, err
+	}
+	user = users[0]
+	return user, nil
+}
+
+// Get all users
+func (u *UsersDB) GetAll() (users []User, err error) {
+	err = u.client.ReadDocuments(u.coll.Self, &users)
+	return
+}
+
+// Create user
+func (u *UsersDB) Add(user *User) (err error) {
+	return u.client.CreateDocument(u.coll.Self, user)
+}
+
+// Update user by id
+func (u *UsersDB) Update(id string, user *User) (err error) {
+	var users []User
+	if err = u.client.QueryDocuments(u.coll.Self,
+		Sprintf("SELECT * FROM ROOT r WHERE r.id='%s'", id), &users); err != nil || len(users) == 0 {
+		return
+	}
+	nUser := users[0]
+	user.Id = nUser.Id
+	if err = u.client.ReplaceDocument(nUser.Self, &user); err != nil {
+		return
+	}
+	return
+}
+
+// Remove user by id
+func (u *UsersDB) Remove(id string) (err error) {
+	var users []User
+	if err = u.client.QueryDocuments(u.coll.Self,
+		Sprintf("SELECT * FROM ROOT r WHERE r.id='%s'", id), &users); err != nil || len(users) == 0 {
+		return
+	}
+	nUser := users[0]
+	if err = u.client.DeleteDocument(nUser.Self); err != nil {
+		return
+	}
+	return
+}
+
+// Find or create collection by id
+func (u *UsersDB) findOrCreateCollection(name string) (err error) {
+	if colls, err := u.client.QueryCollections(u.db.Self, Sprintf("SELECT * FROM ROOT r WHERE r.id='%s'", name)); err != nil {
+		return err
+	} else if len(colls) == 0 {
+		if coll, err := u.client.CreateCollection(u.db.Self, Sprintf(`{ "id": "%s" }`, name)); err != nil {
+			return err
+		} else {
+			u.coll = coll
+		}
+	} else {
+		u.coll = &colls[0]
+	}
+	return
+}
+
+// Find or create database by id
+func (u *UsersDB) findOrDatabase(name string) (err error) {
+	if dbs, err := u.client.QueryDatabases(Sprintf("SELECT * FROM ROOT r WHERE r.id='%s'", name)); err != nil {
+		return err
+	} else if len(dbs) == 0 {
+		if db, err := u.client.CreateDatabase(Sprintf(`{ "id": "%s" }`, name)); err != nil {
+			return err
+		} else {
+			u.db = db
+		}
+	} else {
+		u.db = &dbs[0]
+	}
+	return
 }
